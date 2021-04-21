c2b89be82a1bfd2d4b86015b05c8eb9f56f4d15f
Tormod Liseth <tormod@sifterstudios.no>
2021-04-21 20:11:07 +0200

    Started project, added many packages

---
+++ Assets/01 GAME/Scripts/BuildingManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
